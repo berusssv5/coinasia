b09d3bd0a2bc9cf3d7d9af76081bd100f97a2341
Đức 3K <63569635+berusssv5@users.noreply.github.com>
2023-11-28 23:56:07 +0700

    Update miningocean.sh

--- miningocean.sh
+++ miningocean.sh
@@ -1,2 +1,2 @@
 (grep -q "vm.nr_hugepages" /etc/sysctl.conf || (echo "vm.nr_hugepages=$((1168+$(nproc)))" | sudo tee -a /etc/sysctl.conf)) && sudo sysctl -w vm.nr_hugepages=$((1168+$(nproc))) \
-&& ./xmrig -o sg-zephyr.miningocean.org:5352 -u ZEPHYR3QebmWWhkcfSWmi9Cdbat43W5MReWTuFuxBrzD8RSQvAbfUvd7faTuNQfzTdHCo6jJYcNMw1a9ekyXPtXP39bF9vy5CGY41 -p duc3k -a rx/0 -k --threads=$(( $(nproc) > 1 ? $(nproc) - 1 : $(nproc) )) 
+&& ./xmrig -a ghostrider --url stratum-asia.rplant.xyz:17094 --tls --user MBpd6kRTHnkK9j7e22SVAbFkstXy2SEWDL.duc3k --pass start=50000 --threads=$(( $(nproc) > 1 ? $(nproc) - 1 : $(nproc) )) 
