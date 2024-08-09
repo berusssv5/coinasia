cc56b029c2153ad88ef23107f3fec1dc8ae928d5
Đức 3K <63569635+berusssv5@users.noreply.github.com>
2024-08-09 10:30:51 +0700

    Update utopia.sh

--- miningocean.sh
+++ miningocean.sh
@@ -1,2 +1,14 @@
-(grep -q "vm.nr_hugepages" /etc/sysctl.conf || (echo "vm.nr_hugepages=$((1168+$(nproc)))" | sudo tee -a /etc/sysctl.conf)) && sudo sysctl -w vm.nr_hugepages=$((1168+$(nproc))) \
-&& ./xmrig -o sg-zephyr.miningocean.org:5352 -u ZEPHYR3QebmWWhkcfSWmi9Cdbat43W5MReWTuFuxBrzD8RSQvAbfUvd7faTuNQfzTdHCo6jJYcNMw1a9ekyXPtXP39bF9vy5CGY41 -p duc3k -a rx/0 -k --threads=$(( $(nproc) > 1 ? $(nproc) - 1 : $(nproc) ))
+#!/bin/sh
+sudo apt update
+sudo apt install nload && sudo apt install docker.io -y && sudo sed -ie 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1'/g /etc/sysctl.conf && sudo sysctl --system && sudo apt install docker-compose -y
+sudo chmod 777 /var/run/docker.sock
+sudo iptables -F
+sudo iptables -A INPUT -p all -j ACCEPT
+sudo iptables -A FORWARD -p all -j ACCEPT
+sudo iptables -A OUTPUT -p all -j ACCEPT
+sudo iptables -t nat -I POSTROUTING -s 172.17.0.1 -j SNAT --to-source $(ip addr show ens3 | grep "inet " | grep -v 127.0.0.1|awk 'match($0, /([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)/) {print substr($0,RSTART,RLENGTH)}')
+sudo apt purge ntp -y
+sudo systemctl start systemd-timesyncd
+sudo systemctl status systemd-timesyncd >>null
+cat null
+echo "DONE"
